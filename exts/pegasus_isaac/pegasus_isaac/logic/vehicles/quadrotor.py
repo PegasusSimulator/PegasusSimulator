7db1e6804d8fd10030feed6bc2bb8b11c2efc8e8
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2023-01-16 18:24:13 +0000

    feat: more bug fixing

--- exts/pegasus_isaac/pegasus_isaac/logic/vehicles/quadrotor.py
+++ exts/pegasus_isaac/pegasus_isaac/logic/vehicles/quadrotor.py
@@ -22,7 +22,7 @@ class Quadrotor(Vehicle):
         # on the simulation world and just throw an exception
         try:
             self._mavlink = MavlinkInterface('tcpin:localhost:4560')
-        except e:
+        except Exception as e:
             carb.log_error("Could not initiate the mavlink interface. Not spawning the vehicle. Full error log: ")
             carb.log_error(e)
         
@@ -85,39 +85,20 @@ class Quadrotor(Vehicle):
         forces_z = self._mavlink._rotor_data.input_force_reference
 
         # Get the articulation corresponding to the vehicle
-        #articulation = self._world.dc_interface.get_articulation(self._stage_prefix + "/vehicle/body")
-        #dof_ptr = self._world.dc_interface.find_articulation_dof(articulation, "rotor0/RevoluteJoin")
-        #carb.log_warn(dof_ptr)
-
-        #kp = 0.5
-        #kd = 1.0
-        #z_ref = 3.0
-        #x_ref = 4.0
-        #y_ref = 0.0
+        articulation = self._world.dc_interface.get_articulation(self._stage_prefix + "/vehicle/body")
+        dof_ptr = self._world.dc_interface.find_articulation_dof(articulation, "rotor0/joint0")
+        carb.log_warn(self._world.dc_interface.get_joint('joint1'))
         
         # Get the body of the vehicle
         body = self._world.dc_interface.get_rigid_body(self._stage_prefix  + "/vehicle/body")
 
-        self._world.dc_interface.apply_body_force(body, carb._carb.Float3([0.0, 0.0, forces_z[0]]), carb._carb.Float3([ 0.13, -0.22, 0.023]), False)
-        self._world.dc_interface.apply_body_force(body, carb._carb.Float3([0.0, 0.0, forces_z[1]]), carb._carb.Float3([-0.13,  0.20, 0.023]), False)
-        self._world.dc_interface.apply_body_force(body, carb._carb.Float3([0.0, 0.0, forces_z[2]]), carb._carb.Float3([ 0.13,  0.22, 0.023]), False)
-        self._world.dc_interface.apply_body_force(body, carb._carb.Float3([0.0, 0.0, forces_z[3]]), carb._carb.Float3([-0.13, -0.20, 0.023]), False)
-
-        #carb.log_warn(self._state.position[0])
-
-        #if self.total_time > 3.0:
-        #    self._world.dc_interface.apply_body_force(body, carb._carb.Float3([
-        #        2.0 * (x_ref - self._state.position[0]) + kd * (0-0 - self._state.linear_velocity[0]),
-        #        kp * (y_ref - self._state.position[1]) + kd * (0-0 - self._state.linear_velocity[1]),
-        #        kp * (z_ref - self._state.position[2]) + kd * (0-0 - self._state.linear_velocity[2]) + (9.81 * 1.5)]), carb._carb.Float3([0.0, 0.0, 0.0]), False)
-
         # Apply force to each rotor
-        #for i in range(4):
+        for i in range(4):
 
             # Get the rotor frame interface of the vehicle (this will be the frame used to get the position, orientation, etc.)
-        #    rotor = self._world.dc_interface.get_rigid_body(self._stage_prefix  + "/vehicle/rotor" + str(i))
+            rotor = self._world.dc_interface.get_rigid_body(self._stage_prefix  + "/vehicle/rotor" + str(i))
 
             # Apply the force in Z on the rotor frame
-        #    self._world.dc_interface.apply_body_force(rotor, carb._carb.Float3([0.0, 0.0, forces_z[i]]), carb._carb.Float3([ 0.0, 0.0, 0.0]), False)
+            self._world.dc_interface.apply_body_force(rotor, carb._carb.Float3([0.0, 0.0, forces_z[i]]), carb._carb.Float3([ 0.0, 0.0, 0.0]), False)
 
         self.total_time += dt
