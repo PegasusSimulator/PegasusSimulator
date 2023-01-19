Fix rotation (#1)
* feat: sending groundtruth simulation data through mavlink

* fix: fix a wrong rotation in the accelerometer

* fix: bug in imu where the previous velocity used to compute the acceleration was not being updated

* fix: bug in NVidia simulator gives angular velocity in the inertial frame... YUP, you read it correctly

* milestone: PD controller with the fixed angular velocity now seems to work. HURRAY

exts/pegasus_isaac/pegasus_isaac/logic/sensors/gps.py
@@ -82,7 +82,10 @@ class GPS:
             'eph': self._eph,
             'epv': self._epv,
             'cog': 0.0,
-            'sattelites_visible': self._sattelites_visible
+            'sattelites_visible': self._sattelites_visible,
+            'latitude_gt': self._origin_latitude,
+            'longitude_gt': self._origin_longitude,
+            'altitude_gt': self._origin_altitude
         }
 
     @property
@@ -112,6 +115,9 @@ class GPS:
         # reproject position with noise into geographic coordinates
         pos_with_noise: np.ndarray = state.position + self._noise_gps_pos # + self._gps_bias
         latitude, longitude = reprojection(pos_with_noise, self._origin_latitude, self._origin_longitude)
+
+        # Compute the values of the latitude and longitude without noise (for groundtruth measurements)
+        latitude_gt, longitude_gt = reprojection(state.position, self._origin_latitude, self._origin_longitude)
 
         # Add noise to the velocity expressed in the world frame
         velocity: np.ndarray = state.linear_velocity #+ self._noise_gps_vel
@@ -147,7 +153,10 @@ class GPS:
             'eph': self._eph,
             'epv': self._epv,
             'cog': 0.0, #cog,
-            'sattelites_visible': self._sattelites_visible
+            'sattelites_visible': self._sattelites_visible,
+            'latitude_gt': latitude_gt,
+            'longitude_gt': longitude_gt, 
+            'altitude_gt': state.position[2] + self._origin_altitude
         }
 
         return self._state

exts/pegasus_isaac/pegasus_isaac/logic/vehicles/quadrotor.py
@@ -1,9 +1,53 @@
 #!/usr/bin/env python
 
+import numpy as np
+from scipy.spatial.transform import Rotation
+
 import carb
 from pegasus_isaac.logic.vehicles.vehicle import Vehicle
 from pegasus_isaac.mavlink_interface import MavlinkInterface
 from pegasus_isaac.logic.sensors import Barometer, IMU, Magnetometer, GPS
+
+def rotate_vector_by_quaternion(v, q):
+    """ quaternion according in the [x,y,z,w] standard """
+
+    v = np.array(v)
+    
+    # Extract the vector part of the quaternion
+    u = np.array([q[0], q[1], q[2]])
+
+    # Extract the scalar part of the quaternion
+    s = q[3]
+
+    # Do the math
+    return 2.0 * np.dot(u, v) * u + (s*s - np.dot(u, u)) * v + 2.0 * s * np.cross(u, v)
+
+def quaternion_to_euler(q):
+    """ quaternion according in the [x,y,z,w] standard """
+
+    x = q[0]
+    y = q[1]
+    z = q[2]
+    w = q[3]
+    rpy = np.array([0.0, 0.0, 0.0])
+
+    # Compute roll
+    rpy[0] = np.arctan2(2 * (w * x + y * z), 1 - 2 * (x * x + y*y))
+    
+    # Compute pitch 
+    sin_pitch = 2 * (w*y - z*x)
+
+    if sin_pitch > 1:
+        sin_pitch = 1
+    elif sin_pitch < -1:
+        sin_pitch = -1
+    
+    rpy[1] = np.arcsin(sin_pitch)
+
+    # Compute yaw
+    rpy[2] = np.arctan2(2 * (w * z + x * y), 1 - 2 * (y * y + z * z))
+
+    return rpy
 
 class Quadrotor(Vehicle):
 
@@ -45,6 +89,7 @@ class Quadrotor(Vehicle):
         self._world.add_physics_callback(self._stage_prefix + "/imu", self.update_imu_sensor)
         self._world.add_physics_callback(self._stage_prefix + "/magnetometer", self.update_magnetometer_sensor)
         self._world.add_physics_callback(self._stage_prefix + "/gps", self.update_gps_sensor)
+        self._world.add_physics_callback(self._stage_prefix + "/mav_state", self.update_sim_state_mav)
 
         # Add a callback to start/stop the mavlink streaming once the play/stop button is hit
         self._world.add_timeline_callback(self._stage_prefix + "/start_stop_sim", self.sim_start_stop)
@@ -62,6 +107,9 @@ class Quadrotor(Vehicle):
 
     def update_gps_sensor(self, dt: float):
         self._mavlink.update_gps_data(self._gps.update(self._state, dt))
+
+    def update_sim_state_mav(self, dt: float):
+        self._mavlink.update_sim_state(self._state)
 
     def sim_start_stop(self, event):
         """
@@ -83,6 +131,89 @@ class Quadrotor(Vehicle):
         simulation based on the motor speed. This method must be implemented
         by a class that inherits this type
         """
+        #carb.log_warn(self._state.attitude)
+        #carb.log_warn(self._state.get_attitude_ned_frd())
+
+        # Get the rotor frame interface of the vehicle (this will be the frame used to get the position, orientation, etc.)
+        body = self._world.dc_interface.get_rigid_body(self._stage_prefix)
+
+        kp = 1.0
+        kd = 1.0
+
+        target_pos = [[0.0, 0.0, 1.0], [0.0, 0.0, 1.0], [0.0, 0.0, 1.0], [0.0, 0.0, 1.0]]
+        i = 0
+        if 10.0 <=self.total_time <= 20.0:
+            i = 1
+        elif 20.0 <=self.total_time <= 30.0:
+            i = 2
+        elif 30.0 <=self.total_time:
+            i = 3
+
+        r = Rotation.from_quat(self._state.attitude)
+
+        control = [
+            kp * (0.0 - self._state.position[0]) + kd * (0.0 - self._state.linear_velocity[0]), 
+            kd * (0.0 - self._state.linear_velocity[1]),
+            9.81 * 1.5 + kp * (1.5 - self._state.position[2]) + kd * (0.2 - self._state.linear_velocity[2])
+        ] 
+
+        rotated_control = r.inv().apply(control)
+
+        self._world.dc_interface.apply_body_force(body, carb._carb.Float3(rotated_control), carb._carb.Float3(0.0, 0.0, 0.0), False)
+        #self._world.dc_interface.apply_body_force(body, carb._carb.Float3(0.0, 0.0, 9.81 * 1.5 + kp * (1.0 - self._state.position[2] + kd * (0.0 - self._state.linear_velocity[2]))), carb._carb.Float3(0.0, 0.0, 0.0), False)
+        
+        #carb.log_warn("Forces")
+        
+        #carb.log_warn(rotated_control / np.linalg.norm(rotated_control))
+
+        # roll = 0.0
+        # pitch = 0.0
+        yaw = np.pi / 3.0
+        att_enu = quaternion_to_euler(self._state.attitude)
+        #carb.log_warn(att_enu)
+
+        if self.total_time >= 10.0:
+            #self._world.dc_interface.apply_body_torque(body, carb._carb.Float3([0.001, 0.0, 0.00]), False)
+
+            self._world.dc_interface.apply_body_torque(body, carb._carb.Float3([
+            0.01 * (np.pi/4 - att_enu[0]) + 0.01 * (0.0 - self._state.angular_velocity[0]),
+            0.01 * (0.0 - self._state.angular_velocity[1]),
+            0.01 * (0.0 - self._state.angular_velocity[2])
+            ]),
+            False)
+
+        # carb.log_warn([
+        #     0.01 * (np.pi/4 - att_enu[0]) + 0.01 * (0.0 - self._state.angular_velocity[0]),
+        #     0.01 * (0.0 - self._state.angular_velocity[1]),
+        #     0.01 * (0.0 - self._state.angular_velocity[2])
+        #     ])
+
+        # Apply force to each rotor
+        # for i in range(4):
+        #     pass
+
+            # Get the rotor frame interface of the vehicle (this will be the frame used to get the position, orientation, etc.)
+            #rotor = self._world.dc_interface.get_rigid_body(self._stage_prefix  + "/vehicle/rotor" + str(i))
+
+            # Apply the force in Z on the rotor frame
+            #self._world.dc_interface.apply_body_force(rotor, carb._carb.Float3([0.0, 0.0, forces_z[i]]), carb._carb.Float3([ 0.0, 0.0, 0.0]), False)
+
+            # Rotate the joint to yield the visual of a rotor spinning (for animation purposes only)
+            #joint = self._world.dc_interface.find_articulation_dof(articulation, "joint" + str(i))
+
+            ## Spinning when armed but not applying force
+            #if 0.0 < forces_z[i] < 0.1:
+            #    self._world.dc_interface.set_dof_velocity(joint, 5 * self.rot_dir[i])
+            # Spinning when armed and applying force
+            #elif 0.1 <= forces_z[i]:
+            #    self._world.dc_interface.set_dof_velocity(joint, 100 * self.rot_dir[i])
+            ## Not spinning    
+            #else:
+            #    self._world.dc_interface.set_dof_velocity(joint, 0)
+
+        #carb.log_warn(forces_z)
+
+        self.total_time += dt
 
         self._mavlink.mavlink_update(dt)
 
@@ -90,40 +221,6 @@ class Quadrotor(Vehicle):
         forces_z = self._mavlink._rotor_data.input_force_reference
 
         # Get the articulation root of the vehicle
-        articulation = self._world.dc_interface.get_articulation(self._stage_prefix  + "/vehicle/body")
+        #articulation = self._world.dc_interface.get_articulation(self._stage_prefix)
 
         # Log the orientation
-        #carb.log_warn(self._state.attitude)
-        #carb.log_warn(self._state.get_attitude_ned_frd())
-
-        # Get the rotor frame interface of the vehicle (this will be the frame used to get the position, orientation, etc.)
-        body = self._world.dc_interface.get_rigid_body(self._stage_prefix  + "/vehicle/body")
-
-        #if self.total_time <= 2.0:
-        #self._world.dc_interface.apply_body_force(body, carb._carb.Float3([10.0, 0.0, 0.0]), carb._carb.Float3([0.0, 0.1, 0.0]), False)
-
-        # Apply force to each rotor
-        for i in range(4):
-
-            # Get the rotor frame interface of the vehicle (this will be the frame used to get the position, orientation, etc.)
-            rotor = self._world.dc_interface.get_rigid_body(self._stage_prefix  + "/vehicle/rotor" + str(i))
-
-            # Apply the force in Z on the rotor frame
-            self._world.dc_interface.apply_body_force(rotor, carb._carb.Float3([0.0, 0.0, forces_z[i]]), carb._carb.Float3([ 0.0, 0.0, 0.0]), False)
-
-            # Rotate the joint to yield the visual of a rotor spinning (for animation purposes only)
-            joint = self._world.dc_interface.find_articulation_dof(articulation, "joint" + str(i))
-
-            # Spinning when armed but not applying force
-            if 0.0 < forces_z[i] < 0.1:
-                self._world.dc_interface.set_dof_velocity(joint, 5 * self.rot_dir[i])
-            # Spinning when armed and applying force
-            elif 0.1 <= forces_z[i]:
-                self._world.dc_interface.set_dof_velocity(joint, 100 * self.rot_dir[i])
-            # Not spinning    
-            else:
-                self._world.dc_interface.set_dof_velocity(joint, 0)
-
-        #carb.log_warn(forces_z)
-
-        self.total_time += dt

exts/pegasus_isaac/pegasus_isaac/logic/vehicles/vehicle.py
@@ -13,10 +13,16 @@ from omni.isaac.core.utils.prims import define_prim
 from omni.physx import get_physx_interface
 from omni.usd import get_stage_next_free_path
 from omni.isaac.core.robots.robot import Robot
+from omni.isaac.core.objects import DynamicCuboid
 
 # Extension APIs
 from pegasus_isaac.logic.state import State
 from pegasus_isaac.logic.vehicles.vehicle_manager import VehicleManager
+
+from scipy.spatial.transform import Rotation
+
+# TODO - check if this is viable solution
+from omni.physx.scripts.physicsUtils import add_rigid_box
 
 def get_world_transform_xform(prim: Usd.Prim):
     """
@@ -88,26 +94,38 @@ class Vehicle(Robot):
         # Save the name with which the vehicle will appear in the stage
         # and the name of the .usd file that contains its description
         self._stage_prefix = get_stage_next_free_path(self._current_stage, stage_prefix, False)
-        self._usd_file = usd_path
+        # self._usd_file = usd_path
         
-        # Spawn the vehicle primitive in the world's stage
-        self._prim = define_prim(self._stage_prefix, "Xform")
-        self._prim.GetReferences().AddReference(self._usd_file)
+        # # Spawn the vehicle primitive in the world's stage
+        # self._prim = define_prim(self._stage_prefix, "Xform")
+        # self._prim.GetReferences().AddReference(self._usd_file)
 
-        # Initialize the "Robot" class
-        # Note: we need to change the rotation to have qw first, because NVidia
-        # does not keep a standard of quaternions inside its own libraries (not good, but okay)
-        super().__init__(
-            prim_path=self._stage_prefix, 
-            name=self._stage_prefix, 
-            position=init_pos, 
-            orientation=[init_orientation[3], init_orientation[0], init_orientation[1], init_orientation[2]], 
-            articulation_controller=None
-        )
+        # # Initialize the "Robot" class
+        # # Note: we need to change the rotation to have qw first, because NVidia
+        # # does not keep a standard of quaternions inside its own libraries (not good, but okay)
+        # super().__init__(
+        #    prim_path=self._stage_prefix, 
+        #    name=self._stage_prefix, 
+        #    position=init_pos, 
+        #    orientation=[init_orientation[3], init_orientation[0], init_orientation[1], init_orientation[2]], 
+        #    articulation_controller=None
+        # )
+
+        
+        fancy_cube =  world.scene.add(
+        DynamicCuboid(
+           prim_path=self._stage_prefix,
+           name="fancy_cube",
+           position=np.array(init_pos),
+           scale=np.array([0.32, 0.48, 0.1]),
+           orientation=[init_orientation[3], init_orientation[0], init_orientation[1], init_orientation[2]],
+           color=np.array([0, 0, 1.0]),
+           mass=1.5
+        ))
 
         # Add this object for the world to track, so that if we clear the world, this object is deleted from memory and
         # as a consequence, from the VehicleManager as well
-        self._world.scene.add(self)
+        #self._world.scene.add(self)
 
         # Get a physics interface, so that we can apply forces and torques directly to the rigid body
         self._physx_interface = get_physx_interface()
@@ -163,15 +181,13 @@ class Vehicle(Robot):
         Method that when called, applies a given force vector to the rigid body or /<rigid_body_name>/"body"
         specified.
         """
-        #carb.log_info(self._stage_prefix + body_part + "  " + str(force))
 
-        self._physx_interface.apply_force_at_pos(
-            self._stage_prefix + body_part, carb._carb.Float3(force), carb._carb.Float3(pos))
+        self._physx_interface.apply_force_at_pos(self._stage_prefix + body_part, carb._carb.Float3(force), carb._carb.Float3(pos))
 
     def update_current_state(self, dt):
 
         # Get the body frame interface of the vehicle (this will be the frame used to get the position, orientation, etc.)
-        body = self._world.dc_interface.get_rigid_body(self._stage_prefix  + "/vehicle/body")
+        body = self._world.dc_interface.get_rigid_body(self._stage_prefix)
 
         # Get the current position and orientation in the inertial frame
         pose = self._world.dc_interface.get_rigid_body_pose(body)
@@ -181,10 +197,13 @@ class Vehicle(Robot):
         # But seriously, NVidia, if you are reading this, please fix it and let me know. Robotics people like me do not expect
         # this behaviour from the get_rigid_body_pose method. It is only giving the me initial orientation the vehicle was spawned with
         # Get the attitude according to the convention [w, x, y, z] using the internal Pixar library instead
-        prim = self._world.stage.GetPrimAtPath(self._stage_prefix + "/vehicle/body")
-        rotation_quat = get_world_transform_xform(prim).GetQuaternion()
-        rotation_quat_real = rotation_quat.GetReal()
-        rotation_quat_img = rotation_quat.GetImaginary()
+        # prim = self._world.stage.GetPrimAtPath(self._stage_prefix)
+        # carb.log_warn("-------------#----------")
+        # carb.log_warn(self._world.scene.get_object(self._stage_prefix))
+        # carb.log_warn("-------------#----------")
+        # rotation_quat = get_world_transform_xform(prim).GetQuaternion()
+        # rotation_quat_real = rotation_quat.GetReal()
+        # rotation_quat_img = rotation_quat.GetImaginary()
 
         # Get the angular velocity of the vehicle expressed in the body frame of reference
         ang_vel = self._world.dc_interface.get_rigid_body_angular_velocity(body)
@@ -197,23 +216,28 @@ class Vehicle(Robot):
 
         # Get the linear acceleration of the body relative to the inertial frame, expressed in the inertial frame
         # Note: we must do this approximation, since the Isaac sim does not output the acceleration of the rigid body directly
-        # TODO - check if this is being computed correctly
-        linear_acceleration = (self._state.linear_velocity - np.array(linear_vel)) / dt
+        linear_acceleration = (np.array(linear_vel) - self._state.linear_velocity) / dt
 
         # Update the state variable
         self._state.position = np.array(pose.p)
+        self._state.attitude = np.array(pose.r)
+
+        #carb.log_warn(self._state.position)
 
         # Get the quaternion according in the [x,y,z,w] standard
-        self._state.attitude = np.array([rotation_quat_img[0], rotation_quat_img[1], rotation_quat_img[2], rotation_quat_real])
+        # self._state.attitude = np.array([rotation_quat_img[0], rotation_quat_img[1], rotation_quat_img[2], rotation_quat_real])
 
         # Log the orientation of the vehicle in ENU
-        carb.log_warn(quaternion_to_euler(self._state.attitude))
-        carb.log_warn(self._state.attitude)
+        #carb.log_warn(quaternion_to_euler(self._state.attitude))
+        #carb.log_warn(self._state.attitude)
 
         self._state.linear_body_velocity = np.array(linear_vel_body)
         self._state.linear_velocity = np.array(linear_vel)
-
-        self._state.angular_velocity = np.array(ang_vel)
+        
+        # NOTE: for some reason, NVIDIA gives the angular velocity in a reference frame that is aligned with
+        # the inertial frame, instead of the vehicle body frame, so we need to manually rotate it.. ARrrr
+        # NVidia, let me know when you fix this. I spent way too many hours debugging, because of this. You can do better!
+        self._state.angular_velocity = Rotation.from_quat(self._state.attitude).inv().apply(np.array(ang_vel))
         self._state.linear_acceleration = linear_acceleration
 
     def apply_forces(self, dt: float):

exts/pegasus_isaac/pegasus_isaac/mavlink_interface.py
@@ -76,27 +76,17 @@ class SensorMsg:
         self.vision_covariance = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
         # Simulation State
-        self._new_sim_state: bool = False
-        self._sim_atttitude = [1.0, 0.0, 0.0, 0.0]    # [w, x, y, z]
-        self._sim_acceleration = [0.0, 0.0, 0.0]      # [x,y,z body acceleration]
-        self._sim_angular_vel = [0.0, 0.0, 0.0]       # [roll-rate, pitch-rate, yaw-rate] rad/s
-        self._sim_lat = 0.0                           # [deg]
-        self._sim_lon = 0.0                           # [deg]
-        self._sim_alt = 0.0                           # [m]
-        self._sim_velocity_inertial = [0.0, 0.0, 0.0] # North-east-down [m/s]
-        
-    def update_sim_state(self, state: State):
+        self.new_sim_state: bool = False
+        self.sim_attitude = [1.0, 0.0, 0.0, 0.0]    # [w, x, y, z]
+        self.sim_acceleration = [0.0, 0.0, 0.0]      # [x,y,z body acceleration]
+        self.sim_angular_vel = [0.0, 0.0, 0.0]       # [roll-rate, pitch-rate, yaw-rate] rad/s
+        self.sim_lat = 0.0                           # [deg]
+        self.sim_lon = 0.0                           # [deg]
+        self.sim_alt = 0.0                           # [m]
+        self.sim_ind_airspeed = 0.0                  # Indicated air speed
+        self.sim_true_airspeed = 0.0                 # Indicated air speed
+        self.sim_velocity_inertial = [0.0, 0.0, 0.0] # North-east-down [m/s]
 
-        # Get the quaternion in the convention [x, y, z, w]
-        attitude = state.get_attitude_ned_frd()
-
-        # Rotate the quaternion to the mavlink standard
-        self._sim_atttitude[0] = attitude[3]
-        self._sim_atttitude[1] = attitude[0]
-        self._sim_atttitude[2] = attitude[1]
-        self._sim_atttitude[3] = attitude[2]
-
-        # TODO - keep on going here
 
 class ThrusterControl:
     """
@@ -257,6 +247,11 @@ class MavlinkInterface:
         # Signal that we have new GPS data
         self._sensor_data.new_gps_data = True
 
+        # Also update the groundtruth for the latitude and longitude
+        self._sensor_data.sim_lat = int(data["latitude_gt"] * 10000000)
+        self._sensor_data.sim_lon = int(data["longitude_gt"] * 10000000)
+        self._sensor_data.sim_alt = int(data["altitude_gt"] * 1000)
+
     def update_bar_data(self, data):
         
         # Barometer data
@@ -289,6 +284,46 @@ class MavlinkInterface:
         
         # Signal that we have new vision or mocap data
         self._sensor_data.new_vision_data = True
+
+    def update_sim_state(self, state: State):
+
+        # Get the quaternion in the convention [x, y, z, w]
+        attitude = state.get_attitude_ned_frd()
+
+        # Rotate the quaternion to the mavlink standard
+        self._sensor_data.sim_attitude[0] = attitude[3]
+        self._sensor_data.sim_attitude[1] = attitude[0]
+        self._sensor_data.sim_attitude[2] = attitude[1]
+        self._sensor_data.sim_attitude[3] = attitude[2]
+
+        # Get the angular velocity
+        ang_vel = state.get_angular_velocity_frd()
+        self._sensor_data.sim_angular_vel[0] = ang_vel[0]
+        self._sensor_data.sim_angular_vel[1] = ang_vel[1]
+        self._sensor_data.sim_angular_vel[2] = ang_vel[2]
+
+        # Get the acceleration
+        acc_vel = state.get_linear_acceleration_ned()
+        self._sensor_data.sim_acceleration[0] = int(acc_vel[0] * 1000)
+        self._sensor_data.sim_acceleration[1] = int(acc_vel[1] * 1000)
+        self._sensor_data.sim_acceleration[2] = int(acc_vel[2] * 1000)
+
+        # Get the latitude, longitude and altitude directly from the GPS
+
+        # Get the linear velocity of the vehicle in the inertial frame
+        lin_vel = state.get_linear_velocity_ned()
+        self._sensor_data.sim_velocity_inertial[0] = int(lin_vel[0] * 100)
+        self._sensor_data.sim_velocity_inertial[1] = int(lin_vel[1] * 100)
+        self._sensor_data.sim_velocity_inertial[2] = int(lin_vel[2] * 100)
+
+        # Compute the air_speed - assumed indicated airspeed due to flow aligned with pitot (body x)
+        body_vel = state.get_linear_body_velocity_ned_frd()
+        self._sensor_data.sim_ind_airspeed = int(body_vel[0] * 100)
+        self._sensor_data.sim_true_airspeed = int(np.linalg.norm(lin_vel) * 100) # TODO - add wind here
+
+        self._sensor_data.new_sim_state = True
+
+        carb.log_warn("running")
 
     def __del__(self):
 
@@ -389,10 +424,10 @@ class MavlinkInterface:
         self.send_sensor_msgs(self._current_utime)
 
         # Send the GPS messages
-        self.send_gps_msgs(self._current_utime)        
+        #self.send_gps_msgs(self._current_utime)        
 
         # Send groundtruth
-        self.send_ground_truth()
+        #self.send_ground_truth(self._current_utime)
 
     def poll_mavlink_messages(self):
         """
@@ -553,10 +588,36 @@ class MavlinkInterface:
         except:
             carb.log_warn("Could not send vision/mocap data through mavlink")
 
-    def send_ground_truth(self):
-        # TODO
-        # hil_state_quaternion_send(self, time_usec, attitude_quaternion, rollspeed, pitchspeed, yawspeed, lat, lon, alt, vx, vy, vz, ind_airspeed, true_airspeed, xacc, yacc, zacc)
-        pass
+    def send_ground_truth(self, time_usec: int):
+        
+        carb.log_info("Sending groundtruth msgs")
+
+        # Do not send vision/mocap data, if not new data was received
+        if not self._sensor_data.new_sim_state or self._sensor_data.sim_alt == 0:
+            return
+
+        self._sensor_data.new_sim_state = False
+
+        try:
+            self._connection.mav.hil_state_quaternion_send(
+                time_usec, 
+                self._sensor_data.sim_attitude,
+                self._sensor_data.sim_angular_vel[0],
+                self._sensor_data.sim_angular_vel[1], 
+                self._sensor_data.sim_angular_vel[2], 
+                self._sensor_data.sim_lat,
+                self._sensor_data.sim_lon,
+                self._sensor_data.sim_alt,
+                self._sensor_data.sim_velocity_inertial[0],
+                self._sensor_data.sim_velocity_inertial[1],
+                self._sensor_data.sim_velocity_inertial[2],
+                self._sensor_data.sim_ind_airspeed, 
+                self._sensor_data.sim_true_airspeed,
+                self._sensor_data.sim_acceleration[0],
+                self._sensor_data.sim_acceleration[1], 
+                self._sensor_data.sim_acceleration[2])
+        except:
+            carb.log_warn("Could not send groundtruth through mavlink")
 
     def handle_control(self, time_usec, controls, mode, flags):
         """