2a7deaf502263e61933e9eeb1db790a55c455085
Marcelo Jacinto <marcelo.jacinto@tecnico.ulisboa.pt>
2024-02-03 12:42:50 +0000

    fix: python 3.7 requirement in setup.py

--- extensions/pegasus.simulator/setup.py
+++ extensions/pegasus.simulator/setup.py
@@ -35,7 +35,7 @@ setup(
     keywords=EXTENSION_TOML_DATA["package"]["keywords"],
     license="BSD-3-Clause",
     include_package_data=True,
-    python_requires=">=3.7.*",
+    python_requires=">=3.7",
     install_requires=INSTALL_REQUIRES,
     packages=["pegasus.simulator"],
     classifiers=["Natural Language :: English", "Programming Language :: Python :: 3.7"],
