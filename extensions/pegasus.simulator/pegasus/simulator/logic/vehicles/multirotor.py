927485d80c725e7f7e1ed481a72357802e53dee8
Filip Kuruc <filip.kuruc@airvolute.com>
2024-04-26 12:29:51 +0200

    feat: camera sensor

--- extensions/pegasus.simulator/pegasus/simulator/logic/vehicles/multirotor.py
+++ extensions/pegasus.simulator/pegasus/simulator/logic/vehicles/multirotor.py
@@ -82,7 +82,12 @@ class Multirotor(Vehicle):
         # 2. Initialize all the vehicle sensors
         self._sensors = config.sensors
         for sensor in self._sensors:
-            sensor.initialize(PegasusInterface().latitude, PegasusInterface().longitude, PegasusInterface().altitude)
+            if sensor.sensor_type in ["Camera"]:
+                sensor.initialize(self)
+            else:
+                sensor.initialize(
+                    PegasusInterface().latitude, PegasusInterface().longitude, PegasusInterface().altitude
+                )
 
         # Add callbacks to the physics engine to update each sensor at every timestep
         # and let the sensor decide depending on its internal update rate whether to generate new data
