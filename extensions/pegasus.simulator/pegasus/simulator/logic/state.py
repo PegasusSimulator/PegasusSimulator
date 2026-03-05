0be7b62004a69663372b02be6ca5b63873365608
Rodrigo Gomes <rodrigofpgomes@gmail.com>
2026-03-05 14:04:46 +0000

    feat: State now uses torch tensors (#128)

--- extensions/pegasus.simulator/pegasus/simulator/logic/state.py
+++ extensions/pegasus.simulator/pegasus/simulator/logic/state.py
@@ -6,9 +6,16 @@
 """
 __all__ = ["State"]
 
-import numpy as np
-from scipy.spatial.transform import Rotation
-from pegasus.simulator.logic.rotations import rot_ENU_to_NED, rot_FLU_to_FRD
+
+# import numpy as np
+import torch
+
+#from scipy.spatial.transform import Rotation
+from rotations2 import TorchRotation as Rotation
+
+#from pegasus.simulator.logic.rotations import rot_ENU_to_NED, rot_FLU_to_FRD
+from rotations2 import rot_ENU_to_NED, rot_FLU_to_FRD
+
 
 
 class State:
@@ -24,30 +31,33 @@ class State:
         - linear acceleration - An array with [x_ddot, y_ddot, z_ddot] with the acceleration of the vehicle expressed in the inertial frame according to an ENU convention.
     """
 
-    def __init__(self):
+    def __init__(self, device):
         """
         Initialize the State object
         """
 
+        # Define the same device that is running the simulation
+        self.device = device
+
         # The position [x,y,z] of the vehicle's body frame relative to the inertial frame, expressed in the inertial frame
-        self.position = np.array([0.0, 0.0, 0.0])
+        self.position = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float32, device=self.device)
 
         # The attitude (orientation) of the vehicle's body frame relative to the inertial frame of reference,
         # expressed in the inertial frame. This quaternion should follow the convention [qx, qy, qz, qw], such that "no rotation"
         # equates to the quaternion=[0, 0, 0, 1]
-        self.attitude = np.array([0.0, 0.0, 0.0, 1.0])
+        self.attitude = torch.tensor([0.0, 0.0, 0.0, 1.0], dtype=torch.float32, device=self.device)
 
         # The linear velocity [u,v,w] of the vehicle's body frame expressed in the body frame of reference
-        self.linear_body_velocity = np.array([0.0, 0.0, 0.0])
+        self.linear_body_velocity = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float32, device=self.device)
 
         # The linear velocity [x_dot, y_dot, z_dot] of the vehicle's body frame expressed in the inertial frame of reference
-        self.linear_velocity = np.array([0.0, 0.0, 0.0])
+        self.linear_velocity = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float32, device=self.device)
 
         # The angular velocity [wx, wy, wz] of the vehicle's body frame relative to the inertial frame, expressed in the body frame
-        self.angular_velocity = np.array([0.0, 0.0, 0.0])
+        self.angular_velocity = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float32, device=self.device)
 
         # The linear acceleration [ax, ay, az] of the vehicle's body frame relative to the inertial frame, expressed in the inertial frame
-        self.linear_acceleration = np.array([0.0, 0.0, 0.0])
+        self.linear_acceleration = torch.tensor([0.0, 0.0, 0.0], dtype=torch.float32, device=self.device)
 
     def get_position_ned(self):
         """
@@ -57,7 +67,8 @@ class State:
         Returns:
             np.ndarray: A numpy array with the [x,y,z] of the vehicle expressed in the inertial frame according to an NED convention.
         """
-        return rot_ENU_to_NED.apply(self.position)
+        return rot_ENU_to_NED(device=self.device, dtype=torch.float32).apply(self.position)
+
 
     def get_attitude_ned_frd(self):
         """
@@ -67,7 +78,8 @@ class State:
         Returns:
             np.ndarray: A numpy array with the quaternion [qx, qy, qz, qw] that encodes the attitude of the vehicle's FRD body frame, relative to an NED inertial frame, expressed in the NED inertial frame.
         """
-        attitude_frd_ned = rot_ENU_to_NED * Rotation.from_quat(self.attitude) * rot_FLU_to_FRD
+        attitude_frd_ned = rot_ENU_to_NED(device=self.device, dtype=torch.float32) * Rotation.from_quat(self.attitude) * rot_FLU_to_FRD(device=self.device, dtype=torch.float32)
+
         return attitude_frd_ned.as_quat()
 
     def get_linear_body_velocity_ned_frd(self):
@@ -83,7 +95,7 @@ class State:
         linear_acc_body_flu = Rotation.from_quat(self.attitude).inv().apply(self.linear_acceleration)
 
         # Convert the linear acceleration in the body frame expressed in FLU convention to the FRD convention
-        return rot_FLU_to_FRD.apply(linear_acc_body_flu)
+        return rot_FLU_to_FRD(device=self.device, dtype=torch.float32).apply(linear_acc_body_flu)
 
     def get_linear_velocity_ned(self):
         """
@@ -95,6 +107,7 @@ class State:
             np.ndarray: A numpy array with [vx,vy,vz] that defines the velocity of the vehicle expressed in the inertial frame according to a NED convention.
         """
         return rot_ENU_to_NED.apply(self.linear_velocity)
+        
 
     def get_angular_velocity_frd(self):
         """
