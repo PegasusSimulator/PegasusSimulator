4de01b4bcac05f7f1ab89da4d437758ad64cf791
rodrigofpgomes <rodrigofpgomes@gmail.com>
2026-03-08 01:05:22 +0000

    feat: updated rotations and state to be compatible with CUDA

--- extensions/pegasus.simulator/pegasus/simulator/logic/rotations.py
+++ extensions/pegasus.simulator/pegasus/simulator/logic/rotations.py
@@ -4,25 +4,32 @@
 | License: BSD-3-Clause. Copyright (c) 2023, Marcelo Jacinto. All rights reserved.
 | Description: Implements utilitary rotations between ENU and NED inertial frame conventions and FLU and FRD body frame conventions.
 """
-import numpy as np
-from scipy.spatial.transform import Rotation
+#import numpy as np
+import torch
+
+import pytorch3d.transforms as transforms
 
 # Quaternion for rotation between ENU and NED INERTIAL frames
 # NED to ENU: +PI/2 rotation about Z (Down) followed by a +PI rotation around X (old North/new East)
 # ENU to NED: +PI/2 rotation about Z (Up) followed by a +PI rotation about X (old East/new North)
 # This rotation is symmetric, so q_ENU_to_NED == q_NED_to_ENU.
-# Note: this quaternion follows the convention [qx, qy, qz, qw]
-q_ENU_to_NED = np.array([0.70711, 0.70711, 0.0, 0.0])
+# Note: this quaternion follows the convention [qw, qx, qy, qz]
+# q_ENU_to_NED -> [0.0, 0.70711, 0.70711, 0.0]
 
-# A scipy rotation from the ENU inertial frame to the NED inertial frame of reference
-rot_ENU_to_NED = Rotation.from_quat(q_ENU_to_NED)
+# Rotation from the ENU inertial frame to the NED inertial frame of reference
+def rot_ENU_to_NED(device, dtype):
+    q_ENU_to_NED = torch.tensor([0.0, 0.70711, 0.70711, 0.0], dtype=torch.float32, device=device)
+    return transforms.quaternion_to_matrix(q_ENU_to_NED)
 
 # Quaternion for rotation between body FLU and body FRD frames
 # +PI rotation around X (Forward) axis rotates from Forward, Right, Down (aircraft)
 # to Forward, Left, Up (base_link) frames and vice-versa.
 # This rotation is symmetric, so q_FLU_to_FRD == q_FRD_to_FLU.
-# Note: this quaternion follows the convention [qx, qy, qz, qw]
-q_FLU_to_FRD = np.array([1.0, 0.0, 0.0, 0.0])
+# Note: this quaternion follows the convention [qw, qx, qy, qz]
+# q_FLU_to_FRD -> [0.0, 1.0, 0.0, 0.0]
 
-# A scipe rotation from the FLU body frame to the FRD body frame
-rot_FLU_to_FRD = Rotation.from_quat(q_FLU_to_FRD)
+# Rotation from the FLU body frame to the FRD body frame
+def rot_FLU_to_FRD(device, dtype):
+    q_FLU_to_FRD = torch.tensor([0.0, 1.0, 0.0, 0.0], dtype=torch.float32, device=device)
+    return transforms.quaternion_to_matrix(q_FLU_to_FRD)
+
